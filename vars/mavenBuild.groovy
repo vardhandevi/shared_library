8904f2d6b972c9bfa67f001230dbf8973f51cdbc
vardhandevi <58060440+vardhandevi@users.noreply.github.com>
2020-05-31 19:18:56 -0400

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,12 +1,6 @@
  
-def call(){
-
-stage("maven Build"){
-            steps{
-                withMaven(jdk: 'jdk1.8_172', maven: 'mvn363') {
-    // some block
-     sh "mvn -f javademo/pom.xml clean install -Dmaven.test.skip=true"
-                  }
-              }
-         }
-}
+def call(){         
+      withMaven(jdk: 'jdk1.8_172', maven: 'mvn363') {
+      sh "mvn -f javademo/pom.xml clean install -Dmaven.test.skip=true"
+             }
+       }
