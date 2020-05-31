af7b8fbe985807fd6b01f03f2c50785baa109b95
vardhandevi <58060440+vardhandevi@users.noreply.github.com>
2020-05-31 18:49:41 -0400

    added maven build groovy file

---
+++ vars/mavenBuild.groovy
@@ -0,0 +1,11 @@
+ 
+def call(){
+
+stage("maven Build"){
+            steps{
+                withMaven(jdk: 'jdk1.8_172', maven: 'mvn363') {
+    // some block
+     sh "mvn -f javademo/pom.xml clean install -Dmaven.test.skip=true"
+                  }
+              }
+         }
